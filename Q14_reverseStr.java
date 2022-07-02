2ad470165616b55fd8f400581f716e31ecab2af3
Shrijal Gupta <g02.shrijal@gmail.com>
2022-07-02 12:05:11 +0530

    Prog

--- Q14_reverseStr.java
+++ Q14_reverseStr.java
@@ -1,23 +1,30 @@
+/**
+ * Write a program in java to accept a sentence. Display the new Sentence after reversing
+ * the characters of each word.
+ */
+
 import java.util.*;
 class Q14_Reverse{
     String str;
     StringBuffer sb;
+    String[] arr;
     void input(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter sentence");
         str = sc.nextLine();
+        arr = str.split(" ");
+
     }
     void reverse(){
-        sb = new StringBuffer(str);
-        sb.reverse();
-    }
-    void display(){
-        System.out.print("Output: "+ sb);
+        for (int idx = 0; idx < arr.length; idx++) {
+            sb = new StringBuffer(arr[idx]);
+            sb.reverse();
+            System.out.print(sb + " ");
+        }
     }
     public static void main(String[] Args){
         Q14_Reverse rev = new Q14_Reverse();
         rev.input();
         rev.reverse();
-        rev.display();
     }
 }
